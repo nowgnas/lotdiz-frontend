d32e041b098ec83dcd9306165ca76981405ca6b6
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-10-23 00:43:05 +0900

    refactor : remove unnecessary code

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -43,10 +43,6 @@ onBeforeMount(async () => {
 .carousel__item {
   min-height: 200px;
   width: 100%;
-  background-color: var(--vc-clr-primary);
-  color: var(--vc-clr-white);
-  font-size: 20px;
-  border-radius: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -54,12 +50,6 @@ onBeforeMount(async () => {
 
 .carousel__slide {
   padding: 10px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-  border: 5px solid white;
 }
 
 </style>
