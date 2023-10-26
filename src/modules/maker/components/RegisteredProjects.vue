e4479e03bb27c7be283d36c91e0714afcc46ed08
nowgnas <dev.nowgnas@gmail.com>
2023-10-26 12:12:03 +0900

    add: registered project

---
+++ src/modules/maker/components/RegisteredProjects.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class='registered-project-list-title'>
+    <div class='text'>펀딩중인 프로젝트</div>
+  </div>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+@import "../../../assets/css/maker/RegisteredProjectList.css";
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,73 +1,78 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import MainView from '@/views/MainView.vue';
-import ProjectListByCategoryView from '@/views/ProjectListByCategoryView.vue';
-import ProjectDetailsView from '@/views/ProjectDetailsView.vue';
-import LotdealView from '@/views/LotdealView.vue';
-import SpecialExhibitionView from '@/views/SpecialExhibitionView.vue';
-import ProjectRegisterView from "@/views/ProjectRegisterView.vue";
-import DefaultInformation from "@/modules/project/components/DefaultInformation.vue";
-import MakerRegister from "@/modules/project/components/MakerRegister.vue";
-import ProjectInformation from "@/modules/project/components/ProjectInformation.vue";
-import WriteStory from "@/modules/project/components/WriteStory.vue";
-import RegisterProducts from "@/modules/project/components/RegisterProducts.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import MainView from '@/views/MainView.vue'
+import ProjectListByCategoryView from '@/views/ProjectListByCategoryView.vue'
+import ProjectDetailsView from '@/views/ProjectDetailsView.vue'
+import LotdealView from '@/views/LotdealView.vue'
+import SpecialExhibitionView from '@/views/SpecialExhibitionView.vue'
+import ProjectRegisterView from '@/views/ProjectRegisterView.vue'
+import DefaultInformation from '@/modules/project/components/DefaultInformation.vue'
+import MakerRegister from '@/modules/project/components/MakerRegister.vue'
+import ProjectInformation from '@/modules/project/components/ProjectInformation.vue'
+import WriteStory from '@/modules/project/components/WriteStory.vue'
+import RegisterProducts from '@/modules/project/components/RegisterProducts.vue'
+import RegisteredProjects from '@/modules/maker/components/RegisteredProjects.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: MainView
+    },
+    {
+      path: '/projects/category',
+      name: 'projectListByCategory',
+      component: ProjectListByCategoryView
+    },
+    {
+      path: '/projects/details',
+      name: 'projectDetails',
+      component: ProjectDetailsView
+    },
+    {
+      path: '/lotdeal',
+      name: 'lotdeal',
+      component: LotdealView
+    },
+    {
+      path: '/special-exhibition/camping',
+      name: 'specialExhibition',
+      component: SpecialExhibitionView
+    },
+    {
+      path: '/projects',
+      name: 'projectRegister',
+      component: ProjectRegisterView,
+      children: [
         {
-            path: '/',
-            name: 'main',
-            component: MainView
+          path: 'basic-information',
+          component: DefaultInformation
         },
         {
-            path: '/projects/category',
-            name: 'projectListByCategory',
-            component: ProjectListByCategoryView
+          path: 'maker',
+          component: MakerRegister
         },
         {
-            path: '/projects/details',
-            name: 'projectDetails',
-            component: ProjectDetailsView
+          path: 'project-info',
+          component: ProjectInformation
         },
         {
-            path: '/lotdeal',
-            name: 'lotdeal',
-            component: LotdealView
+          path: 'story',
+          component: WriteStory
         },
         {
-            path: '/special-exhibition/camping',
-            name: 'specialExhibition',
-            component: SpecialExhibitionView
-        },
-        {
-            path: '/projects',
-            name: 'projectRegister',
-            component: ProjectRegisterView,
-            children: [
-                {
-                    path: 'basic-information',
-                    component: DefaultInformation
-                },
-                {
-                    path: 'maker',
-                    component: MakerRegister
-                },
-                {
-                    path: 'project-info',
-                    component: ProjectInformation
-                },
-                {
-                    path: 'story',
-                    component: WriteStory
-                },
-                {
-                    path: 'products',
-                    component: RegisterProducts
-                }
-            ]
+          path: 'products',
+          component: RegisterProducts
         }
+      ]
+    },
+    {
+      path: '/maker/projects',
+      component: RegisteredProjects
+    }
 
-    ]
+  ]
 })
 
 export default router
