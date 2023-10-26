aa1dda7baebb48bff5bbd992a1d74604100a3186
nowgnas <dev.nowgnas@gmail.com>
2023-10-26 20:11:19 +0900

    fix: fix project card component

--- src/modules/maker/components/RegisteredProjects.vue
+++ src/modules/maker/components/RegisteredProjects.vue
@@ -4,11 +4,13 @@ import ProjectCard from '@/modules/maker/components/ProjectCard.vue'
 </script>
 
 <template>
-  <div class='registered-project-list-title'>
-    <div class='text'>펀딩중인 프로젝트</div>
-  </div>
-  <div class='project-list-box'>
-    <ProjectCard />
+  <div class='registered-project-box'>
+    <div class='registered-project-list-title'>
+      <div class='text'>펀딩중인 프로젝트</div>
+    </div>
+    <div class='project-list-box'>
+      <ProjectCard />
+    </div>
   </div>
 </template>
 
