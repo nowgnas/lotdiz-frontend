b4284ad6eefeb5d16d73db20450e9f6ebb9c4606
nowgnas <dev.nowgnas@gmail.com>
2023-10-30 18:11:23 +0900

    fix: unused file delete

--- src/modules/project/components/register-component/RegisterProducts.vue
+++ src/modules/project/components/register-component/RegisterProducts.vue
@@ -4,7 +4,7 @@ import ProjectContentTitle from '@/modules/project/components/register-component
 import SaveButton from '@/modules/project/components/buttons/SaveButton.vue'
 import { ref } from 'vue'
 import ProductItem from '@/modules/project/components/modal/ProductItem.vue'
-import { useProductRegisterStore } from '@/modules/store/registerProjectStore'
+import { useProductRegisterStore } from '@/store/registerProjectStore'
 
 
 const showModal = ref(false)
