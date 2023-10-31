3429cb48b8ecd389381c01a66a27acb7d01ca3ee
CokeLee777 <akak640@kyonggi.ac.kr>
2023-10-31 11:42:39 +0900

    chore: remove unused code

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,6 @@ onMounted(() => {
     headerStore.assignIsNoHeaderPath(false)
   }
 })
-const isNonHeaderPath = (): boolean => {
-  const url: string = window.location.href
-  // const host = ""
-  if (url === 'http://localhost:5173/member/sign-in' || url === 'http://localhost:5173/member/sign-up' || url === 'http://localhost:5173/member/membership-honors/join/success') {
-    return true
-  }
-  return false
-}
 
 </script>
 
